1a3f8cc54edd6d0feb55a7ede6472179e55d5f5f
SilverWords <silverwords12@gmail.com>
2025-05-07 19:20:54 +0000

    functional site

--- resources/js/code.js
+++ resources/js/code.js
@@ -1,16 +1,35 @@
-console.log("Code working");
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+window.onload = function() {
+  var letterContents = document.getElementById("lovelettercontents");
+  
+  fetch('https://southerncharm-146b.restdb.io/rest/lovenotes', {
+    headers: 
+    { 'cache-control': 'no-cache',
+      'x-apikey': '681ba99a72702c62d9b3d4cb' } 
+  })
+  .then(response => response.json())
+  .then(data => {letterContents.innerHTML=data[0].contents; console.log(data)})
+  .catch(error => {console.error('Error:', error); letterContents.innerHTML="An error occured getting the love letter, but I still love you :)"});
 }
+
+function openLetter() {
+  var letter = document.getElementById("loveletter");
+
+  letter.classList.toggle("active");
+  var content = letter.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
+
+/*letter.addEventListener("click", function() {
+  console.log("listener called");
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+});*/
